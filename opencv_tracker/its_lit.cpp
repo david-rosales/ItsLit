9b4141aa005c695c04cef51c707d4c2134cf1e82
Tegra <team16@mit.edu>
2017-02-25 22:49:17 +0000

    Adding some weights to make it stick

--- opencv_tracker/its_lit.cpp
+++ opencv_tracker/its_lit.cpp
@@ -1,43 +1,77 @@
 #include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/opencv.hpp"
+#include <cmath>
 #include <iostream>
 
 using namespace std;
 using namespace cv;
 
-#ifndef CV_VERSION
-#define CV_VERSION "welp"
-#endif
 
 Mat src; Mat bw;
 char window_name1[] = "Unprocessed Image";
 char window_name2[] = "Processed Image";
 
-KeyPoint* maxSizePoint(std::vector<KeyPoint> keypoints)
+double weight_func(KeyPoint * keypoint, KeyPoint * oldMax);
+double distance_between_pts(double x_1, double y_1, double x_2, double y_2);
+
+KeyPoint* maxSizePoint(std::vector<KeyPoint> keypoints, KeyPoint * oldMax)
 {
-    KeyPoint* maxPoint = new KeyPoint(0.0, 0.0, 1);
+    KeyPoint* maxPoint = new KeyPoint(0.0, 0.0, 0.0);
+    double max_weight = 0;
     for (unsigned int i = 0; i < keypoints.size(); i++) {
-      KeyPoint keypoint = keypoints[i];
-      if (keypoint.size > maxPoint->size) {
-        *maxPoint = keypoint;
+      double keypoint_weight = weight_func(&keypoints[i], oldMax);
+      if (keypoint_weight > max_weight) {
+        *maxPoint = keypoints[i];
+        max_weight = keypoint_weight;
       }
     }
     return maxPoint;
 }
+
+// In general
+// weight = (distance from old max * distance_weight) + (point size * size_weight)
+double weight_func(KeyPoint * keypoint, KeyPoint * oldMax) 
+{
+  double distance_weight = 0.40;
+  double distance_numerator = 500000;
+  double size_weight = 0.75;
+
+  Point2f p1 = oldMax->pt;
+  Point2f p2 = keypoint->pt;
+  double total_distance_weight = distance_weight *(distance_numerator / distance_between_pts(p1.x, p1.y, p2.x, p2.y));
+  double total_size_weight = size_weight * keypoint->size;
+  return total_distance_weight + total_size_weight;
+  
+}
+
+
+double distance_between_pts(double x_1, double y_1, double x_2, double y_2)
+{
+  double distance = pow(x_2 - x_1, 2) + pow(y_2 - y_1,2);
+  return pow(distance, 0.5);
+}
+
+
 
 int main( int argc, char** argv )
 {
     // 18.111.40.224
     /// Load the source image
     VideoCapture cap(0);
+    //cap.set(CV_CAP_PROP_EXPOSURE, -2);
+    //cap.set(CV_CAP_PROP_GAIN, 0);
+    //cap.set(CV_CAP_PROP_ISO_SPEED, 0);
+    //cap.set(CV_CAP_PROP_WHITE_BALANCE_U, 0);
+    //cap.set(CV_CAP_PROP_WHITE_BALANCE_V, 0);
+    KeyPoint* mainPoint = new KeyPoint(0.0, 0.0, 0.0);
 
     while(true){
         cap >> src;
 
         //Reduces the visible items
         cv::inRange(src, Scalar(245, 245, 245), Scalar(255, 255, 255), bw);
-        cv::dilate(bw, bw, 0, Point(-1, -1), 3);
+        //cv::dilate(bw, bw, 0);
 
         SimpleBlobDetector::Params params;
 
@@ -73,7 +107,7 @@ int main( int argc, char** argv )
         #endif
 
 
-        KeyPoint* mainPoint = maxSizePoint(keypoints);
+        mainPoint = maxSizePoint(keypoints, mainPoint);
         std::vector<KeyPoint> bestPoints;
         if (mainPoint != NULL) {
 	  bestPoints.push_back((const KeyPoint) *mainPoint);
