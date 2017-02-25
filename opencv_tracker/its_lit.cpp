4c66afaa6254491f39532fe5ff604aa8525d3704
Tegra <team16@mit.edu>
2017-02-25 19:28:27 +0000

    Things running smoothly

--- opencv_tracker/its_lit.cpp
+++ opencv_tracker/its_lit.cpp
@@ -1,9 +1,14 @@
 #include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/opencv.hpp"
+#include <iostream>
 
 using namespace std;
 using namespace cv;
+
+#ifndef CV_VERSION
+#define CV_VERSION "welp"
+#endif
 
 Mat src; Mat dst; Mat hsv; Mat new_image;
 char window_name1[] = "Unprocessed Image";
@@ -15,30 +20,30 @@ int main( int argc, char** argv )
 {
     // 18.111.40.224
     /// Load the source image
-    VideoCapture cap(0);
-    
+    VideoCapture cap(0);    
+
     while(true){
         cap >> src;
 
         //src = imread(argv[1]);
 
-        namedWindow( window_name1, WINDOW_AUTOSIZE );
+        //namedWindow( window_name1, WINDOW_AUTOSIZE );
         //imshow("Unprocessed Image",src);
 
-        dst = src.clone();
-        hsv = src.clone();
+        //dst = src.clone();
+        //hsv = src.clone();
 
         //cv::cvtColor(src, dst, COLOR_BGR2GRAY);
-        cv::cvtColor(src, dst, COLOR_BGR2HSV);
+        //cv::cvtColor(src, dst, COLOR_BGR2HSV);
 
-        cv::inRange(dst, Scalar(0, 0, 245), Scalar(0, 0, 255), hsv);
+        cv::inRange(src, Scalar(245, 245, 245), Scalar(255, 255, 255), hsv);
 
 
         SimpleBlobDetector::Params params;
         
         //Change thresholds
-        params.minThreshold = 10;
-        params.maxThreshold = 2000;
+        params.minThreshold = 100;
+        params.maxThreshold = 200;
 
         params.minDistBetweenBlobs = 20;
 
@@ -52,17 +57,17 @@ int main( int argc, char** argv )
 
         params.filterByArea = true;
         params.minArea = 50;
-        params.maxArea = 20000000;
+        params.maxArea = 2000;
 
         std::vector<KeyPoint> keypoints;
 
         //OpenCV3
-        Ptr<SimpleBlobDetector> detector = SimpleBlobDetector::create(params);
-        detector->detect(hsv, keypoints);
+        //Ptr<SimpleBlobDetector> detector = SimpleBlobDetector::create(params);
+        //detector->detect(hsv, keypoints);
 
         //OpenCV2
-        //SimpleBlobDetector dedector(params);
-        //detector.detect(dst, keypoints);
+        SimpleBlobDetector detector(params);
+        detector.detect(hsv, keypoints);
 
         //namedWindow( window_name2, WINDOW_AUTOSIZE );
         Mat im_with_keypoints;
@@ -72,7 +77,7 @@ int main( int argc, char** argv )
         imshow("keypoints", im_with_keypoints);
         //imshow("Processed Imaged",hsv);
 
-        waitKey(25);
+        waitKey(1);
 
     }
     return 0;
